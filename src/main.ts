5d0baf288826428e3b28c6d683ae114bf825219c
José Pereira Macedo <arseniumgx@gmail.com>
2021-12-07 18:39:42 -0300

    Config swagger on project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('VoidPlus API')
+    .setDescription(`API desenvolvida no decorrer do módulo 4 do curso de 
+    certifucação de desenvolvedores da Blue Edtech, especialidade Fullstack. 
+    Este é o primeiro projeto desenvolvido apenas para o backend.`)
+    .setVersion('1.0.0')
+    .addTag('Users')
+    .addTag('Movies')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/', app, document);
+
+  await app.listen(3001);
 }
 bootstrap();
