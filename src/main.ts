82840d2617becfbb22e235f82ceec2ac4aa5dc79
José Pereira Macedo <arseniumgx@gmail.com>
2021-12-07 18:50:30 -0300

    Config validators to use

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,13 +10,15 @@ async function bootstrap() {
     .setTitle('VoidPlus API')
     .setDescription(`API desenvolvida no decorrer do módulo 4 do curso de 
     certifucação de desenvolvedores da Blue Edtech, especialidade Fullstack. 
-    Este é o primeiro projeto desenvolvido apenas para o backend.`)
+    Este é o primeiro projeto avaliativo do módulo desenvolvido para o backend.`)
     .setVersion('1.0.0')
     .addTag('Users')
     .addTag('Movies')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/', app, document);
+
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(3001);
 }
