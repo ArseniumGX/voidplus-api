46b5a46a699245d553f7d1a33d0e3dacdf249360
José Pereira Macedo <arseniumgx@gmail.com>
2021-12-17 17:45:34 -0300

    Config app to deploy

--- src/main.ts
+++ src/main.ts
@@ -23,8 +23,8 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
   app.useGlobalPipes(new ValidationPipe());
 
-  app.enableCors()
+  app.enableCors();
 
-  await app.listen(3001);
+  await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
